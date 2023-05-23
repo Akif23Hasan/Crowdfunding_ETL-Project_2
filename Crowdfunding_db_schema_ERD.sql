e5a3b7d90bf4547fd4933ee0ba190bb8c8628c39
SurakshaRegmi <128192404+SurakshaRegmi@users.noreply.github.com>
2023-05-23 20:18:12 +1000

    Add files via upload
    ERD Diagram updated

---
+++ Crowdfunding_db_schema_ERD.sql
@@ -0,0 +1,59 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "campaign" (
+    "cf_id" int   NOT NULL,
+    "contact_id" int   NOT NULL,
+    "company_name" Varchar   NOT NULL,
+    "description" Varchar   NOT NULL,
+    "goal" money   NOT NULL,
+    "pledged" money   NOT NULL,
+    "outcome" Varchar   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" Varchar   NOT NULL,
+    "currency" Varchar   NOT NULL,
+    "launch_date" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "category_id" Varchar   NOT NULL,
+    "subcategory_id" Varchar   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "category_id" Varchar   NOT NULL,
+    "category" Varchar   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" int   NOT NULL,
+    "first_name" Varchar   NOT NULL,
+    "last_name" Varchar   NOT NULL,
+    "email" Varchar   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" Varchar   NOT NULL,
+    "subcategory" Varchar   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
+
